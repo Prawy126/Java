c416610c358400cef99caa037ec09ed63f7bb648
Prawy_126 <michal190713@gmail.com>
2023-05-15 12:33:06 +0200

    dokończenie

--- lab 11/src/Main5.java
+++ lab 11/src/Main5.java
@@ -1,6 +1,18 @@
+//zadanmie 4
 public class Main5 {
     public static void main(String[] args){
         Osoba osoba = new Osoba();
-        osoba.setWaga(2.3f);
+        try{
+            osoba.setWaga(1);
+            osoba.setWiek(10);
+            osoba.setNazwisko("Nowak");
+            osoba.setImie("Jacek");
+            osoba.setWzrost(1.9f);
+        }catch(NegativeNumberException e){
+            System.out.println("Podano nie dodatnią wartość wagi");
+        }catch(NullPointerException e){
+            System.out.println("Wartość nie została podana");
+        }
+        System.out.println(osoba.getWaga());
     }
 }
--- lab 11/src/Osoba.java
+++ lab 11/src/Osoba.java
@@ -6,34 +6,47 @@ public class Osoba {
         return nazwisko;
     }
 
-    public void setNazwisko(String nazwisko) {
-        this.nazwisko = nazwisko;
+    public void setNazwisko(String nazwisko)throws NullPointerException {
+        if (nazwisko==null) {
+            throw new NullPointerException("Nazwisko nie może być puste.");
+        } else {
+            this.nazwisko = nazwisko;
+        }
     }
 
     public String getImie() {
         return imie;
     }
 
-    public void setImie(String imie) {
-        this.imie = imie;
+    public void setImie(String imie) throws NullPointerException {
+        if (imie==null) {
+            throw new NullPointerException("Imię nie może być puste.");
+        } else {
+            this.imie = imie;
+        }
     }
 
     public float getWaga() {
         return waga;
     }
 
-    public void setWaga(float waga) throws NegativeNumberException{
-        if(waga<0) throw new NegativeNumberException("Ujemna waga");
-        else
-        this.waga = waga;
+    public void setWaga(float waga) throws NegativeNumberException {
+        if (waga < 0) {
+            throw new NegativeNumberException("Weight cannot be negative.");
+        } else {
+            this.waga = waga;
+        }
     }
-
     public int getWiek() {
         return wiek;
     }
 
-    public void setWiek(int wiek) {
-        this.wiek = wiek;
+    public void setWiek(int wiek)throws NegativeNumberException {
+        if (wiek < 0) {
+            throw new NegativeNumberException("Wiek nie może być ujemny");
+        } else {
+            this.wiek = wiek;
+        }
     }
 
 
@@ -42,7 +55,11 @@ public class Osoba {
         return wzrost;
     }
 
-    public void setWzrost(float wzrost) {
-        this.wzrost = wzrost;
+    public void setWzrost(float wzrost)throws NegativeNumberException {
+        if (wzrost < 0) {
+            throw new NegativeNumberException("Wzrost nie może być ujemny.");
+        } else {
+            this.wzrost = wzrost;
+        }
     }
 }
