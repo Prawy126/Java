f8a59fbdb79f32b78081505ebcfbba72546d301a
Prawy_126 <michal190713@gmail.com>
2023-06-26 18:35:59 +0200

    Skończono podpunkt C zadania 4

--- Kolokwium/src/Zadanie4/Pracownik.java
+++ Kolokwium/src/Zadanie4/Pracownik.java
@@ -3,8 +3,25 @@ package Zadanie4;
 import Zadanie3.Osoba;
 
 public class Pracownik extends Osoba {
-    String zawod;
-    int staz_pracy;
+    public void setZawod(String zawod) {
+        this.zawod = zawod;
+    }
+
+    public void setStaz_pracy(int staz_pracy) {
+        this.staz_pracy = staz_pracy;
+    }
+
+    private String zawod;
+
+    public String getZawod() {
+        return zawod;
+    }
+
+    public int getStaz_pracy() {
+        return staz_pracy;
+    }
+
+    private int staz_pracy;
     public Pracownik(String zawod, int staz_pracy){
         this.zawod = zawod;
         this.staz_pracy = staz_pracy;
@@ -13,5 +30,11 @@ public class Pracownik extends Osoba {
         super(imie,nazwisko,wiek,nazwaKraju);
         this.staz_pracy=staz_pracy;
         this.zawod = zawod;
+    }
+    @Override
+    public void WyswietlDaneOsoby(){
+        super.WyswietlDaneOsoby();
+        System.out.println("Zawód: " + this.zawod);
+        System.out.println("Liczba godzin stażu: " + this.staz_pracy);
     }
 }
