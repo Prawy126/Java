2bf16cc2a553f100d0a477f89908c53d4a53283d
Prawy_126 <michal190713@gmail.com>
2023-07-27 13:09:22 +0200

    zrobienie GUI do programu obliczającego deltę w Java

--- Biblioteka_graficzna/lekcja3/src/RownianieGUI.java
+++ Biblioteka_graficzna/lekcja3/src/RownianieGUI.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.jar.JarEntry;
 
-public class RownianieGUI extends JFrame {
+public class RownianieGUI extends JFrame implements ActionListener {
     private JTextField tA, tB, tC, tWynik;
     private JLabel lA, lB, lC;
     private JButton bWyjscie, bRozwiaz;
@@ -33,5 +35,34 @@ public class RownianieGUI extends JFrame {
         lC.setBounds(10,100,30,20);
         add(lC);
         tC.setToolTipText("Podaj wpółczynnik c");
+
+        bRozwiaz = new JButton("Rozwiąż równanie");
+        bRozwiaz.setBounds(50,150,150,20);
+        add(bRozwiaz);
+        bRozwiaz.addActionListener(this);
+
+        bWyjscie = new JButton("Wyjście");
+        bWyjscie.setBounds(250,150,100,20);
+        add(bWyjscie);
+        bWyjscie.addActionListener(this);
+
+        tWynik = new JTextField();
+        tWynik.setBounds(50,200,350,20);
+        add(tWynik);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object zrodlo = e.getSource();
+        if(zrodlo==bWyjscie){
+            dispose();
+        }else if(zrodlo == bRozwiaz){
+            int a = Integer.parseInt(tA.getText());
+            int b = Integer.parseInt(tB.getText());
+            int c = Integer.parseInt(tC.getText());
+            RownanieKwadratowe rownanieKwadratowe = new RownanieKwadratowe(a,b,c);
+            String rozwiazanie = rownanieKwadratowe.rozwiaz();
+            tWynik.setText(rozwiazanie);
+        }
     }
 }
