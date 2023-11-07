b3be82ec6a6c757c5c853dbf66b4a7012c593b58
Prawy_126 <michal190713@gmail.com>
2023-11-07 22:29:23 +0100

    Dodanie komentarzy do rozwiązania

--- Algorytmy_Lab/Choroba/src/Main.java
+++ Algorytmy_Lab/Choroba/src/Main.java
@@ -1,18 +1,19 @@
 class Main {
 
-    final static int MAX_LICZBA_OSOB = 100000;
-    static int liczbaChorych = 10;
-    static int liczbaZdrowych = MAX_LICZBA_OSOB - liczbaChorych;
-    static int liczbaOdpornych = 0;
-    static int dzien = 0;
-    static int[] liczbaChorychDziennie = new int[50];
-    static int[] liczbaOzdrowialychDziennie = new int[50];
+    final static int MAX_LICZBA_OSOB = 100000; // Maksymalna liczba osób w populacji
+    static int liczbaChorych = 10; // Początkowa liczba chorych osób
+    static int liczbaZdrowych = MAX_LICZBA_OSOB - liczbaChorych; // Początkowa liczba zdrowych osób
+    static int liczbaOdpornych = 0; // Początkowa liczba odpornych osób
+    static int dzien = 0; // Licznik dni
+    static int[] liczbaChorychDziennie = new int[50]; // Tablica przechowująca liczbę chorych osób każdego dnia
+    static int[] liczbaOzdrowialychDziennie = new int[50]; // Tablica przechowująca liczbę osób, które wyzdrowiały każdego dnia
 
+    // Metoda symulująca rozprzestrzenianie się choroby
     public static int rozprzestrzeniajChorobe(int liczbaChorych, int dzien) {
         liczbaChorychDziennie[dzien] = liczbaChorych;
         liczbaZdrowych -= liczbaChorychDziennie[dzien];
 
-        // Uzupełniamy dane na kolejny dzień, ludzie zdrowieją
+        // Uzupełniamy dane na kolejny dzień, ludzie zdrowieją po 7 dniach
         if (dzien > 6) {
             liczbaOzdrowialychDziennie[dzien] = liczbaChorychDziennie[dzien - 7];
             liczbaChorychDziennie[dzien] = liczbaChorychDziennie[dzien] - liczbaChorychDziennie[dzien - 7];
@@ -31,6 +32,7 @@ class Main {
             }
         }
 
+        // Sprawdzamy, czy liczba chorych jest wystarczająco duża, aby zatrzymać symulację
         if (liczbaChorychDziennie[dzien] * 3 >= liczbaZdrowych + liczbaOdpornych) {
             System.out.println("Dzień: " + dzien + " Chorzy: " + liczbaChorych + " Ludzie: " + liczbaZdrowych + " Odporni: " + liczbaOdpornych);
             System.out.println("Liczba: " + (liczbaChorych + liczbaZdrowych + liczbaOdpornych));
@@ -39,11 +41,13 @@ class Main {
                 System.out.print("[" + liczbaChorychDziennie[i] + "] ");
             }
             System.out.println();
-            return dzien;
-        } else return rozprzestrzeniajChorobe(liczbaChorychDziennie[dzien] * 3, dzien + 1);
+            return dzien; // Zakończenie symulacji i zwrócenie numeru dnia
+        } else {
+            return rozprzestrzeniajChorobe(liczbaChorychDziennie[dzien] * 3, dzien + 1); // Rekurencyjne wywołanie symulacji na kolejny dzień
+        }
     }
 
     public static void main(String[] args) {
-        rozprzestrzeniajChorobe(liczbaChorych, dzien);
+        rozprzestrzeniajChorobe(liczbaChorych, dzien); // Rozpoczęcie symulacji z początkową liczbą chorych i dniem równym 0
     }
 }
