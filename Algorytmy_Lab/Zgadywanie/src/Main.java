df553d570b601aaa9ef466f883f9335ea8213d2a
Prawy_126 <michal190713@gmail.com>
2023-11-07 22:34:12 +0100

    Dodanie komentarzy do rozwiązania

--- Algorytmy_Lab/Zgadywanie/src/Main.java
+++ Algorytmy_Lab/Zgadywanie/src/Main.java
@@ -3,25 +3,26 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         Random random = new Random();
-        int proba = 1000;
-        int liczba = Math.abs(random.nextInt() % proba);
-        boolean zadanie = sprawdzenie(liczba, proba, 0);
-        System.out.println("Czy liczba " + liczba + " została znaleziona? " + zadanie);
+        int proba = 1000; // Zakres do poszukiwań
+        int liczba = Math.abs(random.nextInt() % proba); // Losowanie liczby do odgadnięcia
+        boolean zadanie = sprawdzenie(liczba, proba, 0); // Wywołanie funkcji sprawdzającej
+        //System.out.println("Czy liczba " + liczba + " została znaleziona? " + zadanie);
     }
 
+    // Metoda rekurencyjna sprawdzająca, czy podana liczba została znaleziona w danym zakresie
     public static boolean sprawdzenie(int liczba, int proba, int strona) {
         int pomoc = proba / 2 + strona;
         System.out.println("Czy podana liczba to " + pomoc);
 
         if (liczba == pomoc) {
-            System.out.println("Liczba " + pomoc + " jest liczbą wylosowaną wylosowaną" + liczba);
-            return true;
+            System.out.println("Liczba " + pomoc + " jest liczbą wylosowaną " + liczba);
+            return true; // Znaleziono liczbę
         } else if (liczba < pomoc) {
             System.out.println("Liczba " + pomoc + " jest liczbą za dużą");
-            return sprawdzenie(liczba, proba / 2, strona);
+            return sprawdzenie(liczba, proba / 2, strona); // Wywołanie rekurencyjne dla mniejszej połowy zakresu
         } else {
             System.out.println("Liczba " + pomoc + " jest liczbą za małą" );
-            return sprawdzenie(liczba, proba / 2, pomoc + 1);
+            return sprawdzenie(liczba, proba / 2, pomoc + 1); // Wywołanie rekurencyjne dla większej połowy zakresu
         }
     }
 }
