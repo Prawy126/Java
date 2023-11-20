34c358df795ae773f289d5261838bc819c3e8894
Prawy_126 <michal190713@gmail.com>
2023-11-20 16:54:56 +0100

    Rozwiązanie problemu pleckaowego za pomocą programowania dynamicznego

---
+++ Algorytmy_Lab/Zadania_Kolokwium/src/Programowanie_Dynamiczne/A1_Plecak_Dynamicznie.java
@@ -0,0 +1,27 @@
+package Programowanie_Dynamiczne;
+
+public class A1_Plecak_Dynamicznie {
+    public static void main(String[] args) {
+        final int[] objetosci = {6, 2, 3, 2, 3, 1};
+        final int[] wartosci = {6, 4, 5, 7, 10, 2};
+        final int MAX = 10;
+        final int iloscPrzedmiotow = objetosci.length;
+        int[][] tablicaWynikow = new int[iloscPrzedmiotow][MAX+1];
+
+        for (int i=0; i<iloscPrzedmiotow; i++) {
+            for (int j=0; j<MAX+1; j++) {
+                if (i == 0 && objetosci[i] > j) {
+                    tablicaWynikow[i][j] = 0;
+                } else if (i == 0 && objetosci[i] <= j) {
+                    tablicaWynikow[i][j] = wartosci[i];
+                } else if (i > 0 && objetosci[i] > j) {
+                    tablicaWynikow[i][j] = tablicaWynikow[i-1][j];
+                } else if (i > 0 && objetosci[i] <= j) {
+                    tablicaWynikow[i][j] = Math.max(tablicaWynikow[i - 1][j], wartosci[i] + tablicaWynikow[i - 1][j - objetosci[i]]);
+                }
+            }
+        }
+
+        System.out.println("Maksymalna wartość plecaka to " + tablicaWynikow[iloscPrzedmiotow-1][MAX] + " przy pojemności " + MAX);
+    }
+}
