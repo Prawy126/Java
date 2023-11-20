5b1d010157952aa05d9bc289ba6c4ee04be9ea38
Prawy_126 <michal190713@gmail.com>
2023-11-20 16:22:36 +0100

    Losowanie liczby i zgadywanie metodą dziel i zwyciężaj

---
+++ Algorytmy_Lab/Zadania_Kolokwium/src/Dziel_Zwyciezaj/A6_Losowanie_Liczby.java
@@ -0,0 +1,27 @@
+package Dziel_Zwyciezaj;
+import java.util.Random;
+public class A6_Losowanie_Liczby {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int proba = 1000;
+        int liczba = Math.abs(random.nextInt() % proba);
+        boolean zadanie = sprawdzenie(liczba, proba, 0);
+        //System.out.println("Czy liczba " + liczba + " została znaleziona? " + zadanie);
+    }
+    public static boolean sprawdzenie(int liczba, int proba, int strona) {
+        int pomoc = proba / 2 + strona;
+        System.out.println("Czy podana liczba to " + pomoc);
+
+        if (liczba == pomoc) {
+            System.out.println("Liczba " + pomoc + " jest liczbą wylosowaną " + liczba);
+            return true;
+        } else if (liczba < pomoc) {
+            System.out.println("Liczba " + pomoc + " jest liczbą za dużą");
+            return sprawdzenie(liczba, proba / 2, strona);
+        } else {
+            System.out.println("Liczba " + pomoc + " jest liczbą za małą");
+            return sprawdzenie(liczba, proba / 2, pomoc + 1);
+        }
+    }
+}
