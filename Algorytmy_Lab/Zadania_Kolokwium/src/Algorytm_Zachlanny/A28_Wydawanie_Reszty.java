a066e53482b4de04a4ab1b3055240e18f823d62b
Prawy_126 <michal190713@gmail.com>
2023-11-21 20:24:04 +0100

    Update

--- Algorytmy_Lab/Zadania_Kolokwium/src/Algorytm_Zachlanny/A28_Wydawanie_Reszty.java
+++ Algorytmy_Lab/Zadania_Kolokwium/src/Algorytm_Zachlanny/A28_Wydawanie_Reszty.java
@@ -1,36 +1,36 @@
 package Algorytm_Zachlanny;
 
 public class A28_Wydawanie_Reszty {
-    public static void main(String[] args) {
-        final int[] nominaly = {1,2,5,10,20,50,100,200,500};
-        int[] dostepneMonety = {30,7,4,1,5,3,0,1,7};
-        int[] wydaneMonety = new int[9];
-        final double RESZTA = 7.53;
-        int resztaWgroszach = (int) (RESZTA * 100);
-        //tutaj należy odwrócić pętlę
-        for (int i = dostepneMonety.length-1; i >= 0; i--) {
-            int nominal = nominaly[i];
-            int iloscDostepnych = dostepneMonety[i];
+    private static final int[] NOMINAŁY = {1, 2, 5, 10, 20, 50, 100, 200, 500};
+    private static final int[] DOSTĘPNE_MONETY = {5, 3, 4, 1, 0, 7, 0, 1, 3};
 
-            while (resztaWgroszach >= nominal && iloscDostepnych > 0) {
-                resztaWgroszach -= nominal;
-                iloscDostepnych--;
-                wydaneMonety[i]++;
+    public static void wydajResztę(int złote, int grosze) {
+        int pozostałaKwota = złote * 100 + grosze;
+
+        for (int i = NOMINAŁY.length - 1; i >= 0; i--) {
+            int liczbaMonetDoWydania = 0;
+            while (pozostałaKwota >= NOMINAŁY[i] && DOSTĘPNE_MONETY[i] > 0) {
+                pozostałaKwota -= NOMINAŁY[i];
+                DOSTĘPNE_MONETY[i]--;
+                liczbaMonetDoWydania++;
+            }
+
+            if (liczbaMonetDoWydania > 0) {
+                System.out.println("Nominał: " + NOMINAŁY[i]/100.0 + "zł, Liczba monet: " + liczbaMonetDoWydania);
             }
         }
 
-        System.out.println("Wydane monety:");
-        for (int i = 0; i < nominaly.length; i++) {
-            int zlotowki = nominaly[i] / 100;
-            int grosze = nominaly[i] % 100;
-            System.out.println(zlotowki + " zł " + grosze + " gr \t" + wydaneMonety[i]);
-        }
-
-        if (resztaWgroszach > 0) {
-            System.out.println("Nie udało się wydać całej reszty, zostało " + resztaWgroszach + " gr");
-        } else {
-            System.out.println("Udało się wydać całą resztę");
+        if (pozostałaKwota > 0) {
+            System.out.println("Nie można wydać dokładnej reszty. Zostało: " + pozostałaKwota/100.0 + "zł");
         }
     }
+
+    public static void main(String[] args) {
+        int resztaZłote = 9;
+        int resztaGrosze = 28;
+        System.out.println("Wydawanie reszty dla kwoty: " + resztaZłote + "zł " + resztaGrosze + "gr");
+        wydajResztę(resztaZłote, resztaGrosze);
+    }
+
 }
 
--- Algorytmy_Lab/Zadania_Kolokwium/src/Monte_Carlo/Pole_Figury.java
+++ Algorytmy_Lab/Zadania_Kolokwium/src/Monte_Carlo/Pole_Figury.java
@@ -8,14 +8,15 @@ public class Pole_Figury {
         int pole = 0;
 
         for(int i=0; i<100; i++){
+            //tutaj wpisz ile losowych punktów
             double x = (double) rand.nextInt(100+1)/100;
             double y = (double) rand.nextInt(100+1)/100;
-
+            //tutaj wpisz wzór
             if(y<=Math.pow(x,2)){
                 pole++;
             }
         }
-
+                        //stosunek zlicznonego pola do ilości losów
         System.out.println("Pole figury: " + (float) pole / 100);
     }
 }
